ba0bf3b5df64ca843761bfb96e1576a83819c3ff
Jens Raaby <git@jensraaby.com>
2016-07-11 22:15:27 +0100

    Add webpack for building Javascript/CSS and bundling into the docroot.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: "./main.js",
+    output: {
+        path: "src/main/resources/js/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    },
+    devServer: {
+        inline: true,
+        port: 3333
+    }
+};
